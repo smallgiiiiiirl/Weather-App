ce13e5b623363980bf747d54107856fb27669ff7
smallgiiiiiirl <96083874+smallgiiiiiirl@users.noreply.github.com>
2023-01-17 22:28:21 +0300

    Revert "Problems with context & fetch"

--- src/types/Weather.ts
+++ src/types/Weather.ts
@@ -2,10 +2,11 @@ export interface Weather{
     weathers: {id: number, main: string, description: string, }[],
     main: {temp: number, feels_like: number,pressure: number, humidity: number},
     visibility: number,
-    wind: {speed: number},
+    wind: {speed: number, deg: number},
     sys:{type: number, id: number,country: string}
     timezone: number,
     id: number,
     name: string,
     cod: number
+
 }
